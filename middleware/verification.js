d3a0e79d640323a5b71784cfe0929f0043dae340
dhanvanthgs25@gmail.com <dhanvanthgs25@gmail.com>
2025-11-05 19:48:46 +0530

    verification for registration

---
+++ middleware/verification.js
@@ -0,0 +1,46 @@
+const expressAsyncHandler = require('express-async-handler');
+const crypto = require('crypto');
+require('dotenv').config();
+
+const verify_payment = expressAsyncHandler(async (req, res,next) => {
+  console.log("Verifying payment with data:", req.body);
+  const { payment_id, order_id, signature } = req.body;
+  const {name , email, phone, college,event} = req.body;
+  const amount = req.body.amount;
+  if(event === 'General_Pass'){
+    if(amount != 89){
+      res.status(400).send("Invalid amount for General Pass,Please contact the admin");
+      return;
+    }
+  }
+  if(event ==='Graphics_Grid' || event ==='Stage_play' || event ==='Still_Of_Soul' || event ==='AdaptTune' || event ==='Quizcorn' ){
+    if(amount != 99){
+      res.status(400).send("Invalid amount for selected event,Please contact the admin");
+      return;
+    }
+  }
+
+  if(event ==='Script_Writing' || event ==='TrailerCut'){
+    if(amount !=150){
+      res.status(400).send("Invalid amount for selected event,Please contact the admin");
+      return;
+    }
+  }
+
+  if(event ==='CinePlus' ){
+    if(amount != 149){
+      res.status(400).send("Invalid amount for CinePlus event,Please contact the admin");
+      return;
+    }
+  }
+  const generated_signature = crypto
+    .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
+    .update(`${order_id}|${payment_id}`)
+    .digest('hex');
+
+  if (generated_signature === signature) {
+    next();
+  } else {
+    res.status(400).send("Payment verification failed");
+  }
+});
--- routers/registration.js
+++ routers/registration.js
@@ -1,8 +1,9 @@
 const express =require('express');
 const router = express.Router();
 const {solo_event,group_event} = require('../controllers/registration');
+const verify_payment = require('../middleware/verification');
 
-router.post('/solo/:event',solo_event);
-router.post('/group/:event',group_event);
+router.post('/solo/:event',verify_payment,solo_event);
+router.post('/group/:event',verify_payment,group_event);
 
 module.exports = router;
