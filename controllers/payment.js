38f5c8bb6413757ac47f6a43db3a1f701dee58e9
dhanvanthgs25@gmail.com <dhanvanthgs25@gmail.com>
2025-11-05 18:57:46 +0530

    ammount verification added

--- controllers/payment.js
+++ controllers/payment.js
@@ -36,7 +36,33 @@ const verify_payment = expressAsyncHandler(async (req, res) => {
   console.log("Verifying payment with data:", req.body);
   const { payment_id, order_id, signature } = req.body;
   const {name , email, phone, college,event} = req.body;
+  const amount = req.body.amount;
+  if(event === 'General_Pass'){
+    if(amount != 89){
+      res.status(400).send("Invalid amount for General Pass,Please contact the admin");
+      return;
+    }
+  }
+  if(event ==='Graphics_Grid' || event ==='Stage_play' || event ==='Still_Of_Soul' || event ==='AdaptTune' || event ==='Quizcorn' ){
+    if(amount != 99){
+      res.status(400).send("Invalid amount for selected event,Please contact the admin");
+      return;
+    }
+  }
 
+  if(event ==='Script_Writing' || event ==='TrailerCut'){
+    if(amount !=150){
+      res.status(400).send("Invalid amount for selected event,Please contact the admin");
+      return;
+    }
+  }
+
+  if(event ==='CinePlus' ){
+    if(amount != 149){
+      res.status(400).send("Invalid amount for CinePlus event,Please contact the admin");
+      return;
+    }
+  }
   const generated_signature = crypto
     .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
     .update(`${order_id}|${payment_id}`)
